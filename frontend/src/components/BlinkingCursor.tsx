9efaffefbabbd138065b17f0934ffc4f567dea15
David Halapir <halapir.david@gmail.com>
2025-06-09 14:22:59 +0200

    simplify animaton

--- frontend/src/components/BlinkingCursor.tsx
+++ frontend/src/components/BlinkingCursor.tsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { Loader } from '@/components/Loader';
-import { Progress } from '@/components/ui/progress';
 
 import { responseProgressState } from 'client-types/*';
 
@@ -17,7 +16,7 @@ export default function BlinkingCursor(_: Props) {
   const { id } = useParams();
   const progressState = useRecoilValue(responseProgressState);
 
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState<number | undefined>(undefined);
 
   const showInteractiveProgress = id === progressState.thread_id;
 
@@ -29,58 +28,15 @@ export default function BlinkingCursor(_: Props) {
     setProgress(parseInt(progressState.percentage, 10));
   }, [progressState, showInteractiveProgress]);
 
-  if (!showInteractiveProgress) {
-    return (
-      <div className="flex gap-2 items-center pt-6">
-        <Loader className="!size-4" />
-        <div className="text-muted-foreground">
-          {messageForProgress(progress)}
-        </div>
-      </div>
-    );
-  }
-  //   <span
-  //   className={cn(
-  //     'inline-block h-3.5 w-3.5 bg-foreground rounded-full animate-pulse',
-  //     whitespace && 'ml-2'
-  //   )}
-  // />
-
   return (
-    <div className="grid gap-1">
-      <div className="flex gap-2 items-center">
-        <Progress value={progress} />
-        <div className="text-sm w-10 shrink-0 text-muted-foreground ">
-          {progress.toFixed(0)}%
-        </div>
-      </div>
-      <div className="flex gap-2 items-center">
-        <Loader className="!size-4" />
-        <div className="text-muted-foreground">
-          {messageForProgress(progress)}
-        </div>
+    <div className="flex gap-2 items-center pt-6">
+      <Loader className="!size-4" />
+      <div className="flex items-center">
+        <div className="text-muted-foreground">Thinking...</div>
+        {!!progress && (
+          <div className="text-muted-foreground">{progress.toFixed(0)}%</div>
+        )}
       </div>
     </div>
   );
 }
-
-const messageForProgress = (progress: number) => {
-  const messages = {
-    range1: ['Gathering your thoughts...'],
-    range2: ['Connecting the dots...'],
-    anticipation: ['Almost there...']
-  };
-
-  const getRandomMessage = (list: string[]) =>
-    list[Math.floor(Math.random() * list.length)];
-
-  if (progress < 60) {
-    return getRandomMessage(messages.range1);
-  }
-
-  if (progress < 80) {
-    return getRandomMessage(messages.range2);
-  }
-
-  return getRandomMessage(messages.anticipation);
-};
